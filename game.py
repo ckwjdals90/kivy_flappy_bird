519c31688227ed4551c00c0c039c883afee92ed2
Jungmin Cha <ckwjdals90@gmail.com>
2017-06-26 10:57:51 -0700

    instead of rgb, using actual imagery for the game; assets included

--- game.py
+++ game.py
@@ -1,15 +1,19 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
+from kivy.uix.image import Image
 
 
 class Game(Widget):
-    pass
+    def __init__(self):
+        super(Game, self).__init__()
+        self.add_widget(Image(source='images/background.png'))
+        self.add_widget(Image(source='images/bird.png'))
 
 
 class GameApp(App):
     def build(self):
-        return Game(size=Window.size)
+        return Game()
 
 
 if __name__ == "__main__":
