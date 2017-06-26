17bb6cda1601322a90b25082a4fc255694a0e7c6
Jungmin Cha <ckwjdals90@gmail.com>
2017-06-26 10:38:20 -0700

    trying out the kv file

--- game.py
+++ game.py
@@ -1,15 +1,10 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.graphics import Color, Rectangle
 from kivy.core.window import Window
 
 
 class Game(Widget):
-    def __init__(self, **kwargs):
-        super(Game, self).__init__(**kwargs)
-        with self.canvas:
-            Color(.5, .5, 1.0)
-            Rectangle(pos=(0, 0), size=self.size)
+    pass
 
 
 class GameApp(App):
