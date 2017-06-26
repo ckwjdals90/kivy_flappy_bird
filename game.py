8a3404ea203f9115c6f4a876ebe9e0979487f64a
Jungmin Cha <ckwjdals90@gmail.com>
2017-06-26 11:38:23 -0700

    bird interacts with touches, flapping

--- game.py
+++ game.py
@@ -29,18 +29,35 @@ class Background(Widget):
             self.image_dupe.x = self.width
 
 
+class Bird(Sprite):
+    def __init__(self, pos):
+        super(Bird, self).__init__(source='images/bird.png', pos=pos)
+        self.velocity_y = 0
+        self.gravity = -.3
+
+    def update(self):
+        self.velocity_y += self.gravity
+        self.velocity_y = max(self.velocity_y, -10)
+        self.y += self.velocity_y
+
+    def on_touch_down(self, *ignore):
+        self.velocity_y = 5.5
+
+
 class Game(Widget):
     def __init__(self):
         super(Game, self).__init__()
         self.background = Background(source='images/background.png')
         self.size = self.background.size
         self.add_widget(self.background)
-        self.add_widget(Sprite(source='images/bird.png'))
+        self.bird = Bird(pos=(20, self.height / 2))
+        self.add_widget(self.bird)
         # speed of the background animation
         Clock.schedule_interval(self.update, 1.0 / 60.0)
 
     def update(self, *ignore):
         self.background.update()
+        self.bird.update()
 
 
 class GameApp(App):
